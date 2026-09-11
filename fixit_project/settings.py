0a0a1a1a5b780d234af349c311be0f3786dbaa3d
imaninaitore <imaninaitore1726@gmail.com>
2026-09-11 09:25:57 +0300

    add rest_framework and   corsheaders to installed apps

--- fixit_project/settings.py
+++ fixit_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'fixIt_app',
+    "rest_framework",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
