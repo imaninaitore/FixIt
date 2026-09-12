5aa17bfe4edefaa4345297cfe7885fa4d164c094
imaninaitore <imaninaitore1726@gmail.com>
2026-09-12 18:37:29 +0300

    create login view

--- fixit_app/views.py
+++ fixit_app/views.py
@@ -5,8 +5,10 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny #Registration must be available to people who are not logged in yet
 from rest_framework.response import Response#sends data back to the person or application making the request.
 from rest_framework import status #gives readable HTTP status codes
+from rest_framework.views import APIView
 
 from .serializers import RegistrationSerializer
+from .serializers import LoginSerializer
 
 
 # Create your views here.
@@ -31,3 +33,19 @@ def register(request):
         serializer.errors,
         status=status.HTTP_400_BAD_REQUEST,
     )
+
+class LoginView(APIView):
+
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+
+        if serializer.is_valid():
+            return Response(
+                serializer.validated_data,
+                status=status.HTTP_200_OK
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
