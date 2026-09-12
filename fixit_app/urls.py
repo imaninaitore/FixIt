d3babf073f599048973f3e7e22224a5743cb794e
imaninaitore <imaninaitore1726@gmail.com>
2026-09-12 21:53:21 +0300

    add logout url

--- fixit_app/urls.py
+++ fixit_app/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import register,login
+from .views import register,login,logout
 
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", login, name="login"),
+    path("logout/", logout, name="logout"),
 
 ]
