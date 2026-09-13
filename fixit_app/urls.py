6ae475d09f6b6b8a45bbed30d4a53900db13498e
imaninaitore <imaninaitore1726@gmail.com>
2026-09-13 10:42:27 +0300

    add refresh token endpoint

--- fixit_app/urls.py
+++ fixit_app/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 from .views import register,login,logout
 
+from rest_framework_simplejwt.views import TokenRefreshView
+
 
 urlpatterns = [
     path("register/", register, name="register"),
     path("login/", login, name="login"),
     path("logout/", logout, name="logout"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 
 ]
