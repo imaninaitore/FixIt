f8d4d3fe0abd5bed9d91ae981e201fbe63ecb1bd
imaninaitore <imaninaitore1726@gmail.com>
2026-09-14 18:13:01 +0800

    add submit url

--- fixit_app/urls.py
+++ fixit_app/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
-from .views import register,login,logout,my_profile,provider_profile,create_provider_enrolment
+from .views import( register,
+                   login,
+                   logout,
+                   my_profile,
+                   provider_profile,
+                   create_provider_enrolment,
+                   submit_provider_enrolment
+                   )
 
 from rest_framework_simplejwt.views import TokenRefreshView
 
@@ -14,5 +21,5 @@ urlpatterns = [
     path("me/", my_profile, name="my_profile"),
     path("providers/me/", provider_profile, name="provider_profile"),
     path("enrolment/", create_provider_enrolment, name="create_provider_enrolment"),
-
+    path("enrolment/me/submit/",submit_provider_enrolment,name="submit_provider_enrolment"),
 ]
