b70f915708ec8090e275c2366e042f8d33b606f6
imaninaitore <imaninaitore1726@gmail.com>
2026-09-15 03:56:32 +0800

    Update the main project URLs to have path from providers.urls

--- fixit_project/urls.py
+++ fixit_project/urls.py
@@ -20,5 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("api/auth/", include("accounts.urls")),
-     path("api/providers/", include("fixit_app.urls")),
+     path("api/providers/", include("providers.urls")),
 ]
