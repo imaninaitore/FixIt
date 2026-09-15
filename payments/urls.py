03399e4eeb9f3ec682992fb00f608f883fe3338b
imaninaitore <imaninaitore1726@gmail.com>
2026-09-15 19:29:42 +0800

    update urls to add my_payments path

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import initiate_payment
+from .views import initiate_payment,my_payments
 
 
 urlpatterns = [
     path("initiate/", initiate_payment, name="initiate-payment"),
+     path("me/", my_payments, name="my-payments"),
 ]
