cb05a0c8b3f6a3f8e267fa20d273c3290a997753
imaninaitore <imaninaitore1726@gmail.com>
2026-09-15 19:33:19 +0800

    add payments status path to the urls

--- payments/urls.py
+++ payments/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 from .views import ( initiate_payment,
                      my_payments,
                      payment_detail,
+                     payment_status,
                    )
 
 
 urlpatterns = [
     path("initiate/", initiate_payment, name="initiate-payment"),
     path("me/", my_payments, name="my-payments"),
-      path("<int:payment_id>/", payment_detail, name="payment-detail"),
+    path("<int:payment_id>/status/", payment_status, name="payment-status"),
+    path("<int:payment_id>/", payment_detail, name="payment-detail"),
 ]
