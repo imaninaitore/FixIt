0fa6fbbbb7167c4aac4aba9a513abc614a2d8f88
imaninaitore <imaninaitore1726@gmail.com>
2026-09-15 19:31:40 +0800

    create payment-detail" path

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import initiate_payment,my_payments
+from .views import ( initiate_payment,
+                     my_payments,
+                     payment_detail,
+                   )
 
 
 urlpatterns = [
     path("initiate/", initiate_payment, name="initiate-payment"),
-     path("me/", my_payments, name="my-payments"),
+    path("me/", my_payments, name="my-payments"),
+      path("<int:payment_id>/", payment_detail, name="payment-detail"),
 ]
