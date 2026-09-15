730aaae19cd455040956bdc621da1d84cfe8bae6
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 03:23:05 +0800

    add url route for complete service request

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -6,7 +6,8 @@ from .views import (
     provider_service_requests,
     accept_service_request,
     reject_service_request,
-    update_service_request_status
+    update_service_request_status,
+    complete_service_request
 )
 
 
@@ -18,5 +19,6 @@ urlpatterns = [
     path( "<int:request_id>/accept/", accept_service_request, name="accept_service_request" ),
     path( "<int:request_id>/reject/", reject_service_request, name="reject_service_request" ),
     path( "<int:request_id>/status/", update_service_request_status, name="update_service_request_status" ),
+    path( "<int:request_id>/complete/", complete_service_request, name="complete_service_request" ),
     path("<int:request_id>/", service_request_detail, name="service_request_detail"),
 ]
