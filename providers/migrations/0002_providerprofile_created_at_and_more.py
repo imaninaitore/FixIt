dd03c43c82d40556bcef4e84e97adf08426fd4b6
imaninaitore <imaninaitore1726@gmail.com>
2026-09-15 18:11:55 +0800

    run migration

--- providers/migrations/0002_providerprofile_created_at_and_more.py
+++ providers/migrations/0002_providerprofile_created_at_and_more.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='providerprofile',
             name='created_at',
-            field=models.DateTimeField(auto_now_add=True, default=1),
+            field=models.DateTimeField(auto_now_add=True),
         ),
         migrations.AddField(
             model_name='providerprofile',
