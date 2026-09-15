e80e7504f2b1b85ec377a1494fd355cd304c206a
imaninaitore <imaninaitore1726@gmail.com>
2026-09-15 19:06:33 +0800

    correct url path duplicate

--- providers/urls.py
+++ providers/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path("me/", provider_profile, name="provider_profile"),
     path("enrolment/", create_provider_enrolment, name="create_provider_enrolment", ),
     path( "enrolment/me/", my_provider_enrolment, name="my-provider-enrolment" ), 
-    path( "enrolment/me/", update_provider_enrolment, name="update-provider-enrolment" ),
+    path( "enrolment/me/update", update_provider_enrolment, name="update-provider-enrolment" ),
     path( "enrolment/me/submit/",submit_provider_enrolment, name="submit_provider_enrolment", ),
     path( "enrolment/me/withdraw/", withdraw_provider_enrolment, name="withdraw-provider-enrolment" ),
 
