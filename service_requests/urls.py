49bbb0156e09c5f97a4c8bb8bbcc86c99a29cd7a
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 03:03:25 +0800

    add url path

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import create_service_request
+from .views import (create_service_request,
+                    my_service_requests,
+                    )
 
 
 urlpatterns = [
     # Create a new service request.
     path("", create_service_request, name="create_service_request"),
+    path("", my_service_requests, name="my_service_requests"),
 ]
