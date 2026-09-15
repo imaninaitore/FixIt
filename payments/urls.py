bd5ae4ed6135d8270182ae3c4c383de1ed931e80
imaninaitore <imaninaitore1726@gmail.com>
2026-09-15 19:37:04 +0800

    Then add the callback route to urls

--- payments/urls.py
+++ payments/urls.py
@@ -4,12 +4,14 @@ from .views import ( initiate_payment,
                      my_payments,
                      payment_detail,
                      payment_status,
+                     mpesa_callback,
                    )
 
 
 urlpatterns = [
     path("initiate/", initiate_payment, name="initiate-payment"),
     path("me/", my_payments, name="my-payments"),
+    path("mpesa/callback/", mpesa_callback, name="mpesa-callback"),
     path("<int:payment_id>/status/", payment_status, name="payment-status"),
     path("<int:payment_id>/", payment_detail, name="payment-detail"),
 ]
