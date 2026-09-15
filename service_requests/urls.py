d9369223a36349dbfec380a0ae070fb31ad242a0
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 03:15:20 +0800

    Add the provider URL

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import (
     service_requests_list,
     service_request_detail,
+    provider_service_requests,
 )
 
 
@@ -10,5 +11,6 @@ urlpatterns = [
     # GET: View the logged-in customer's own requests.
     # POST: Create a new service request.
     path( "",service_requests_list, name="service_requests_list" ),
+    path( "provider/", provider_service_requests, name="provider_service_requests" ),
     path("<int:request_id>/", service_request_detail, name="service_request_detail"),
 ]
