5c3c27211f646281dc97739952128f2ec74fe148
imaninaitore <imaninaitore1726@gmail.com>
2026-09-17 02:24:41 +0800

    update url

--- payments/urls.py
+++ payments/urls.py
@@ -1,22 +1,10 @@
 from django.urls import path
-
-from .views import ( initiate_payment,
-                     my_payments,
-                     payment_detail,
-                     payment_status,
-                     mpesa_callback,
-                     verify_payment,
-                     mpesa_stk_push
-                   )
-
+from .views import payment_information
 
 urlpatterns = [
-    path("initiate/", initiate_payment, name="initiate-payment"),
-    path("me/", my_payments, name="my-payments"),
-    path("mpesa/callback/", mpesa_callback, name="mpesa-callback"),
-    path("<int:payment_id>/status/", payment_status, name="payment-status"),
-     path("<int:payment_id>/verify/", verify_payment, name="verify-payment"),
-    path("<int:payment_id>/", payment_detail, name="payment-detail"),
-
-    path("mpesa/stk-push/", mpesa_stk_push, name="mpesa-stk-push"),
+    path(
+        "information/",
+        payment_information,
+        name="payment_information"
+    ),
 ]
