36a3139da46c921877fa6184bb55cfc0d4400cf8
imaninaitore <imaninaitore1726@gmail.com>
2026-09-17 02:56:20 +0800

    correct submit url path

--- providers/urls.py
+++ providers/urls.py
@@ -5,12 +5,12 @@ from .views import (
    create_provider_enrolment, 
    my_provider_enrolment, 
    update_provider_enrolment, 
-   submit_provider_enrolment, 
    withdraw_provider_enrolment,
 
     provider_directory,
     public_provider_profile,
-    provider_categories
+    provider_categories ,
+    submit_enrolment, 
 )
 
 urlpatterns = [
@@ -19,7 +19,7 @@ urlpatterns = [
     path("enrolment/", create_provider_enrolment, name="create_provider_enrolment", ),
     path( "enrolment/me/", my_provider_enrolment, name="my-provider-enrolment" ), 
     path( "enrolment/me/update/", update_provider_enrolment, name="update-provider-enrolment" ),
-    path( "enrolment/me/submit/",submit_provider_enrolment, name="submit_provider_enrolment", ),
+    path( "enrolment/submit/",submit_enrolment, name="submit_enrolment", ),
     path( "enrolment/me/withdraw/", withdraw_provider_enrolment, name="withdraw-provider-enrolment" ),
 
     path("", provider_directory, name="provider-directory"),
