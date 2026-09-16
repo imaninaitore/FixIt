0fc3e4f8c482c1872837eb49614db525fe0070cd
imaninaitore <imaninaitore1726@gmail.com>
2026-09-17 02:57:15 +0800

    Register the models in Django Admin

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,37 @@
 from django.contrib import admin
+from .models import Payment, SubscriptionPlan
 
-# Register your models here.
+
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = (
+        "provider",
+        "enrolment",
+        "amount",
+        "transaction_code",
+        "status",
+        "payment_date",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "plan",
+        "payment_date",
+    )
+
+    search_fields = (
+        "provider__username",
+        "transaction_code",
+        "phone_number",
+    )
+
+
+@admin.register(SubscriptionPlan)
+class SubscriptionPlanAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "amount",
+        "duration_days",
+        "is_active",
+    )
--- providers/admin.py
+++ providers/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import ProviderProfile, ProviderEnrolment
 
-# Register your models here.
+
+@admin.register(ProviderProfile)
+class ProviderProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        "business_name",
+        "user",
+        "service_category",
+        "location",
+        "is_available",
+    )
+
+
+@admin.register(ProviderEnrolment)
+class ProviderEnrolmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "business_name",
+        "provider",
+        "service_category",
+        "status",
+        "payment_status",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "payment_status",
+        "service_category",
+    )
+
+    search_fields = (
+        "business_name",
+        "provider__username",
+        "provider__email",
+        "payment_reference",
+    )
