6108ab8e928b3bff8eef72c2d4c5dd2f28dcfbd0
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 17:46:24 +0800

    move relevant  models from fixitapp to administation app

--- fixit_app/models.py
+++ fixit_app/models.py
@@ -1,20 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Account(models.Model):
-    ACCOUNT_TYPES = (
-        ("customer", "Customer"),
-        ("provider", "provider"),
-     )
-
-    user = models.OneToOneField( User, on_delete=models.CASCADE)
-
-    account_type = models.CharField( max_length=20,choices=ACCOUNT_TYPES )
-
-    def __str__(self):
-        return f"{self.user.username} - {self.account_type}"
-
+# Create your models here
 
 # Stores additional information about a service provider
 class ProviderProfile(models.Model):
