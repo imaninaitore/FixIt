96cc5aee0bb4ac6e24b32bb15aec6f8bfb1c90e9
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 17:37:02 +0800

    add the url route

--- administration/urls.py
+++ administration/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 
-from .views import admin_users
+from .views import (admin_users,
+                    admin_user_detail,
+
+                    )
 
 
 urlpatterns = [
     path(  "users/",  admin_users,  name="admin-users" ),
+     path( "users/<int:user_id>/",admin_user_detail,name="admin-user-detail" ),
 ]
