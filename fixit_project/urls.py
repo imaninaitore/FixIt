0e9b3d231ac8c7c50ae1b6a0111472cc4d787b73
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 16:34:43 +0800

    connect reviews app urls to main project

--- fixit_project/urls.py
+++ fixit_project/urls.py
@@ -23,4 +23,5 @@ urlpatterns = [
     path("api/providers/", include("providers.urls")),
     path("api/payments/", include("payments.urls")),
     path("api/requests/", include("service_requests.urls")),
+    path( "api/", include("reviews.urls")),
 ]
