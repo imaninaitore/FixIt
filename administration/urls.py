0f74c00c9348c03d6a5f62d4c5894dfbfa2f7e22
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 17:39:02 +0800

    add admin_user_status url path

--- administration/urls.py
+++ administration/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 
 from .views import (admin_users,
                     admin_user_detail,
+                    admin_user_status,
 
                     )
 
@@ -9,4 +10,5 @@ from .views import (admin_users,
 urlpatterns = [
     path(  "users/",  admin_users,  name="admin-users" ),
      path( "users/<int:user_id>/",admin_user_detail,name="admin-user-detail" ),
+      path( "users/<int:user_id>/status/", admin_user_status, name="admin-user-status"),
 ]
