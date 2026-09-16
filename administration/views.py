8d57a0fa1f222ab8590ad2edab47f0d092caf787
imaninaitore <imaninaitore1726@gmail.com>
2026-09-16 17:33:10 +0800

    create admin users view function

--- administration/views.py
+++ administration/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
 
-# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAdminUser
+from rest_framework.response import Response
+from rest_framework import status
+
+
+@api_view(["GET"])
+@permission_classes([IsAdminUser])
+def admin_users(request):
+    users = User.objects.all().order_by("-date_joined")
+
+    user_data = []
+
+    for user in users:
+        user_data.append(
+            {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "is_active": user.is_active,
+                "is_staff": user.is_staff,
+                "date_joined": user.date_joined,
+            }
+        )
+
+    return Response(
+        {
+            "count": len(user_data),
+            "users": user_data,
+        },
+        status=status.HTTP_200_OK
+    )
