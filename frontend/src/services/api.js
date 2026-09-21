f51c7b7e4494fbf5809645dfcf182dd7a5a20401
imaninaitore <imaninaitore1726@gmail.com>
2026-09-21 11:13:00 +0300

    importing the function from our central API service.

---
+++ frontend/src/services/api.js
@@ -0,0 +1,11 @@
+const API_URL = "http://127.0.0.1:8000/api"; //base address of your Django API.
+
+export async function getProviders() {//creates a function that React can use whenever it needs providers.
+
+    const response = await fetch(`${API_URL}/providers/`); //make a request to your existing Django endpoint
+    if (!response.ok) {
+        throw new Error("Failed to fetch providers");
+    }
+
+    return response.json();
+}
