18bb278e214e3db3204070629ae18a042579eba5
imaninaitore <imaninaitore1726@gmail.com>
2026-09-21 09:13:13 +0300

    add home path and view function

--- fixit_project/urls.py
+++ fixit_project/urls.py
@@ -17,7 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 
+from .views import home
+
 urlpatterns = [
+    path("", home, name="home"),
     path('admin/', admin.site.urls),
     path("api/auth/", include("accounts.urls")),
     path("api/providers/", include("providers.urls")),
