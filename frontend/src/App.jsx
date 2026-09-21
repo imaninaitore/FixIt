d03e8bc37d4ee51b54e400f49950042e361b4caf
imaninaitore <imaninaitore1726@gmail.com>
2026-09-21 11:32:10 +0300

    connect home.jsx to app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,7 @@
-import { useEffect, useState } from "react";
-import { getProviders } from "./services/api";
+import Home from "/pages/Home";
 
 function App() {
-    const [providers, setProviders] = useState([]);
-    const [error, setError] = useState("");
-
-    useEffect(() => {
-        getProviders()
-            .then((data) => {
-                setProviders(data);
-            })
-            .catch((error) => {
-                setError(error.message);
-            });
-    }, []);
-
-    return (
-        <div>
-            <h1>FixIt Providers</h1>
-
-            {error && <p>{error}</p>}
-
-            {providers.map((provider) => (
-                <div key={provider.id}>
-                    <h2>{provider.business_name}</h2>
-                    <p>{provider.service_category}</p>
-                    <p>{provider.location}</p>
-                </div>
-            ))}
-        </div>
-    );
+    return <Home />;
 }
 
 export default App;
