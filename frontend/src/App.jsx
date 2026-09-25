919bb5108929da7169027065f7b3ba3584ad8a1c
imaninaitore <imaninaitore1726@gmail.com>
2026-09-25 14:09:04 +0300

    add route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import ProviderEnrolment from "./pages/ProviderEnrolment";
 
 function App() {
     return (
@@ -12,6 +13,7 @@ function App() {
                 <Route path="/" element={<Home />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/register" element={<Register />} />
+                <Route path="/provider/enrolment" element={<ProviderEnrolment />}/>
 
             </Routes>
         </BrowserRouter>
